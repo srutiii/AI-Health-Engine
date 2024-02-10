d0e6d5192a511d47623dc49ecb5d740063407070
Abhishek Prasad <prasadabhi95056@gmail.com>
2024-02-10 11:48:36 +0530

    chatbot added

--- src/components/chatbot/Chatbot.jsx
+++ src/components/chatbot/Chatbot.jsx
@@ -1,37 +1,23 @@
-import Chatbot from "react-chatbot-kit";
-import "react-chatbot-kit/build/main.css";
-
-import React, { useState } from "react";
-import config from "./config";
-import MessageParser from "./MessageParser";
-import ActionProvider from "./ActionProvider";
-
-function Chattbot() {
-  const [isChatbotVisible, setChatbotVisibility] = useState(false);
-
-  const toggleChatbot = () => {
-    setChatbotVisibility(!isChatbotVisible);
-  };
-  return (
-    <div>
-      {/* chatbot icon */}
-      <button
-        className="fixed bottom-6 right-4 w-14 bg-blue-200 p-2 rounded-full cursor-pointer transform transition duration-100 ease-in-out hover:scale-110 z-10"
-        onClick={toggleChatbot}
-      >
-        <img src="src/assets/chatbot-icon.png" alt="img" />
-      </button>
-      <div className="fixed z-10 bottom-20 right-10 scrollbar-hide">
-        {isChatbotVisible && (
-          <Chatbot
-            config={config}
-            messageParser={MessageParser}
-            actionProvider={ActionProvider}
-          />
-        )}
-      </div>
-    </div>
-  );
+import React, { useEffect } from 'react'
+ 
+const Chatbot = () => {
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://cdn.botpress.cloud/webchat/v1/inject.js'
+    script.async = true
+    document.body.appendChild(script)
+ 
+    script.onload = () => {
+      window.botpressWebChat.init({
+        botId: '8bdcb65f-498f-464c-bdeb-ec345ee97476',
+        hostUrl: 'https://cdn.botpress.cloud/webchat/v1',
+        messagingUrl: 'https://messaging.botpress.cloud',
+        clientId: '8bdcb65f-498f-464c-bdeb-ec345ee97476',
+      })
+    }
+  }, [])
+ 
+  return <div id="webchat" />
 }
-
-export default Chattbot;
+ 
+export default Chatbot
--- stash/App.jsx
+++ stash/App.jsx
@@ -9,7 +9,6 @@ import Services from "./Pages/Services";
 import DiseasePrediction from "./Pages/DiseasePrediction";
 import Profile from "./Pages/Profile";
 import Chatbot from "./components/chatbot/Chatbot";
-import DoctorRecommed from "./Pages/DoctorRecommed";
 // import config from "./components/chatbot/config";
 // import MessageParser from "./components/chatbot/MessageParser";
 // import ActionProvider from "./components/chatbot/ActionProvider";
@@ -20,8 +19,9 @@ import DoctorRecommed from "./Pages/DoctorRecommed";
 function App() {
   return (
     <div className="bg-white dark:bg-gray-900 scrollbar-hide overflow-hidden">
+      
       <Nav />
-      {/*<Chattbot />*/}
+      {/* <Chattbot /> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactUs />} />
@@ -31,11 +31,10 @@ function App() {
         <Route path="/predict" element={<DiseasePrediction />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/bot" element={<Chatbot />} />
-        <Route path="/recommend" element={<DoctorRecommed />} />
       </Routes>
       <Nav_sm />
     </div>
   );
 }
-
+ 
 export default App;

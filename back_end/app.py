a05162d77c031f4e737e6130f854f530d5a9dc17
Abhishek70331 <prasadabhi95056@gmail.com>
2024-02-15 22:25:00 +0530

    predictive model for checking the disease

--- back_end/app.py
+++ back_end/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_mysqldb import MySQL
+import pandas as pd
+import numpy as np
+import pickle
+import csv
+from scipy.stats import mode
+import warnings
 
 
 app = Flask(__name__)
@@ -13,6 +19,26 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'helloAI'
  
 mysql = MySQL(app)
+
+#removing warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+with open('models/svm_model.pkl', 'rb') as a:
+    svm_model = pickle.load(a)
+
+with open('models/nb_model.pkl', 'rb') as b:
+    nb_model = pickle.load(b)
+
+with open('models/rf_model.pkl', 'rb') as c:
+    rf_model = pickle.load(c)
+
+with open('models/data_dict.pkl', 'rb') as d:
+    data_dict = pickle.load(d)
+
+with open('models/Doctor_Specialist_Model.pkl', 'rb') as f:
+   specialization = pickle.load(f)
 
 
 
@@ -47,6 +73,35 @@ def signup():
 
     return jsonify({"success": True, "message": "Registration successful"})
 
+
+#Route for diease prediction...
+@app.route('/diseasepredict/<symptoms>',methods = ['GET','POST'])
+def predictDisease(symptoms):
+
+
+    input_data = [0] * len(data_dict["symptom_index"])
+    for symptom in symptoms:
+        index = data_dict["symptom_index"].get(symptom.capitalize(), -1)
+        if index != -1:
+            input_data[index] = 1
+
+    input_data = np.array(input_data).reshape(1, -1)
+    
+    rf_prediction = data_dict["predictions_classes"][rf_model.predict(input_data)[0]]
+    nb_prediction = data_dict["predictions_classes"][nb_model.predict(input_data)[0]]
+    svm_prediction = data_dict["predictions_classes"][svm_model.predict(input_data)[0]]
+
+    final_prediction = mode([rf_prediction, nb_prediction, svm_prediction], axis=0, keepdims=True, nan_policy='omit')[0][0]
+
+    predictions = {
+        "rf_model_prediction": rf_prediction,
+        "nb_model_prediction": nb_prediction,
+        "svm_model_prediction": svm_prediction,
+        "final_prediction": final_prediction
+    }
+    print(predictions)
+    
+    return predictions
 
 if __name__ == '__main__':
     app.run()

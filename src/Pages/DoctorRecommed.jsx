e41eb18029768cf8e09466a6d0bf9e37d9394689
Srutiii <srutidas0228@gmail.com>
2024-02-18 12:29:33 +0530

    lists enhanced

--- src/Pages/DoctorRecommed.jsx
+++ src/Pages/DoctorRecommed.jsx
@@ -1,9 +1,113 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Dropdown, DropdownItem } from "flowbite-react";
 import Doctor_card from '../components/Doctor_card';
 
-
 function DoctorRecommed() {
+  const [specializationFilter, setSpecializationFilter] = useState(null);
+  const [locationFilter, setLocationFilter] = useState(null);
+
+  const Doctors = [
+    {
+      id: 1,
+      name: "Suresh H. Advani",
+      city: "Mumbai",
+      Specialisation: "Oncologist",
+      clinic: "Jaslok Hospital, Mumbai",
+    },
+    {
+      id: 2,
+      name: "Ashok Seth",
+      city: "New Delhi",
+      Specialisation: "Cardiologist",
+      clinic: "Fortis Escorts Heart Institute, New Delhi",
+    },
+    {
+      id: 3,
+      name: "P S Binu",
+      city: "Kochi",
+      Specialisation: "General Surgeon",
+      clinic: "Aster Medcity Kochi",
+    },
+    {
+      id: 4,
+      name: "Sandeep Vaishya",
+      city: "New Delhi",
+      Specialisation: "Neurosurgeon",
+      clinic: "Fortis Escorts Heart Institute, New Delhi",
+    },
+    {
+      id: 5,
+      name: "Harit Chaturvedi",
+      city: "New Delhi",
+      Specialisation: "Oncologist",
+      clinic: "Max Super Speciality Hospital, Saket, New Delhi",
+    },
+    {
+      id: 7,
+      name: "P.K. Gambhir",
+      city: "New Delhi",
+      Specialisation: "General Surgeon",
+      clinic: "Saroj Super Speciality Hospital",
+    },
+    {
+      id: 8,
+      name: "Devi Prasad Shetty",
+      city: "Bangalore",
+      Specialisation: "Cardiac Surgeon",
+      clinic: "Narayana Multispeciality Hospital, Bangalore",
+    },
+    {
+      id: 9,
+      name: "S Radhakrishnan",
+      city: "Gurgaon",
+      Specialisation: "Pediatric Cardiac Surgeon",
+      clinic: "Medanta - The Medicity, Gurgaon",
+    },
+    {
+      id: 10,
+      name: "Basanta Behera",
+      city: "Bhubaneswar",
+      Specialisation: "Orthopaedic",
+      clinic: "AMRI Hospitals, Bhubaneswar",
+    },
+    {
+      id: 11,
+      name: "Pradeep Sen",
+      city: "Kolkata",
+      Specialisation: "Cosmetic Surgeon",
+      clinic: "Apollo Hospital, Kolkata",
+    },
+  ];
+
+  const locations = {
+    Mumbai: "Mumbai",
+    "New Delhi": "New Delhi",
+    Kochi: "Kochi",
+    Bangalore: "Bangalore",
+    Gurgaon: "Gurgaon",
+    Bhubaneswar: "Bhubaneswar",
+    Kolkata: "Kolkata",
+  };
+
+  const specializations = {
+    Oncologist: "Oncologist",
+    Cardiologist: "Cardiologist",
+    "General Surgeon": "General Surgeon",
+    Neurosurgeon: "Neurosurgeon",
+    "Pediatric Cardiac Surgeon": "Pediatric Cardiac Surgeon",
+    Orthopaedic: "Orthopaedic",
+    "Cosmetic Surgeon": "Cosmetic Surgeon",
+    "Cardiac Surgeon": "Cardiac Surgeon",
+  };
+
+  const filteredDoctors = Doctors.filter((doctor) => {
+    return (
+      (!specializationFilter ||
+        doctor.Specialisation === specializationFilter) &&
+      (!locationFilter || doctor.city === locationFilter)
+    );
+  });
+
   return (
     <div className="w-full">
       <div className="">
@@ -22,38 +126,57 @@ function DoctorRecommed() {
         </div>
         <div className="flex px-10 py-3 w-full">
           <div className="px-2 ">
-            <Dropdown label="Location" dismissOnClick={false}>
-              <DropdownItem>Location 1</DropdownItem>
-              <DropdownItem>Location 2</DropdownItem>
-              <DropdownItem>Location 3</DropdownItem>
-              <DropdownItem>Location 4</DropdownItem>
+            <Dropdown
+              label="Location"
+              value={locationFilter}
+              dismissOnClick={false}
+              onChange={(value) => setLocationFilter(value)}
+            >
+              {Object.keys(locations).map((location) => (
+                <DropdownItem
+                  key={location}
+                  value={location}
+                  className="hover:bg-gray-300"
+                >
+                  {location}
+                </DropdownItem>
+              ))}
             </Dropdown>
           </div>
           <div className=" w-full">
-            <Dropdown label="Doctor's Specialization" dismissOnClick={false}>
-              <DropdownItem>Specialization 1</DropdownItem>
-              <DropdownItem>Specialization 2</DropdownItem>
-              <DropdownItem>Specialization 3</DropdownItem>
-              <DropdownItem>Specialization 4</DropdownItem>
+            <Dropdown
+              label="Doctor's Specialization"
+              value={specializationFilter}
+              dismissOnClick={false}
+              onChange={(value) => setSpecializationFilter(value)}
+            >
+              {Object.keys(specializations).map((specialization) => (
+                <DropdownItem
+                  key={specialization}
+                  value={specialization}
+                  className="hover:bg-gray-300"
+                >
+                  {specialization}
+                </DropdownItem>
+              ))}
             </Dropdown>
           </div>
           {/* <div className='flex items-center justify-end w-full text-gray-500 text-sm'>Results 10</div> */}
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 px-3 py-3 overflow-hidden">
-          <Doctor_card name='Paras Nath' location='park street' Specialization='Neuro'/>
-          <Doctor_card />
-          <Doctor_card />
-          <Doctor_card />
-          <Doctor_card />
-          <Doctor_card />
-          <Doctor_card />
-          <Doctor_card />
-          <Doctor_card />
-          <Doctor_card />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 px-3 py-3 overflow-hidden place-items-center">
+          {filteredDoctors.map((doctor) => (
+            <Doctor_card
+              key={doctor.id}
+              name={doctor.name}
+              city={doctor.city}
+              location={doctor.clinic}
+              Specialization={doctor.Specialisation}
+            />
+          ))}
         </div>
       </div>
     </div>
   );
 }
 
-export default DoctorRecommed
+export default DoctorRecommed;
--- src/components/Doctor_card.jsx
+++ src/components/Doctor_card.jsx
@@ -3,37 +3,39 @@ import { Link } from "react-router-dom";
 // import { Button, Modal } from "flowbite-react";
 import { useState } from "react";
 
-function Doctor_card({name, Specialization, location }) {
+function Doctor_card({name, Specialization, city, location }) {
     const [openModal, setOpenModal] = useState(false);
   return (
-    <Card>
+    <Card className="w-[360px] shadow-sm shadow-gray-500  my-5">
       <div className="flex">
-        <div className="overflow-hidden">
+        {/* <div className="overflow-hidden">
           <img
             src="https://assets-global.website-files.com/6426543485efe6a5ade36f21/64eeb730f28ad152d8d18244_Introducing-Dr.-Carewise--Your-Empathetic-3D-Animated-Doctor-gigapixel-standard-scale-6_00x.jpg"
             alt=""
             className="flex w-28 px-2 py-2 items-center"
           />
-        </div>
-        <div>
-          <h5 className="text-2xl font-bold font-playfair tracking-tight text-gray-900 dark:text-white px-2">
-            Dr. {name}
-          </h5>
-          <ul className="font-sans text-gray-700 dark:text-gray-400 px-2 py-2 text-sm text-justify">
+        </div> */}
+        <div className="w-full">
+          <div className="border-b-2 border-gray-200">
+            <h5 className="text-2xl flex justify-center font-bold font-playfair tracking-tight text-gray-900 dark:text-white px-2 ">
+              Dr. {name}
+            </h5>
+          </div>
+          <ul className="font-sans text-gray-700 dark:text-gray-400 px-3 py-2 text-sm text-justify">
             <li>Specialization: {Specialization} </li>
+            <li>City: {city} </li>
             <li>Location: {location} </li>
             <li>Rating: ⭐⭐⭐ </li>
           </ul>
 
-          <div className=" flex  justify-between px-4">
+          <div className=" flex  justify-center items-center ">
             <Link className="px-2 py-2">
               <button
                 onClick={() => setOpenModal(true)}
-                className="bg-blue-300  px-4 py-2 rounded text-sm font-sans hover:bg-cyan-700"
+                className="bg-blue-300  px-4 py-2 rounded text-sm font-sans hover:bg-cyan-700 "
               >
                 Book Appointment
               </button>
-              
             </Link>
             {/* <Link className="px-2 py-2">
               <button className="bg-blue-300  px-4 py-2 rounded text-sm font-sans"></button>
